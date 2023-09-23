b0542ea7e7415df307fe31c8afea5cf5b3678103
agustinibar <agustinibarperrotta@gmail.com>
2023-09-23 20:06:52 -0300

    register controller

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,9 +1,40 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'
 import { Jwt } from 'jsonwebtoken';
+import User from '../models/User';
 
+const register = async(req, res)=>{
+    try {
+        const {
+            firsName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation,
+        } = req.body;
+        
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
 
-const register = async()=>{
-
+        const newUser = new User({
+            firsName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000),
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser)
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 export { register }
