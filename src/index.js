e644e01f9e01f5f067db28a0657855092bcabcfe
agustinibar <agustinibarperrotta@gmail.com>
2023-09-23 16:01:41 -0300

    multer config

--- src/index.js
+++ src/index.js
@@ -8,6 +8,29 @@ import helmet from 'helmet';
 import morgan from "morgan";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import exp from 'constants';
 
-//config
+//server config
 
+const __filename = fileURLToPath(import.meta.url);
+const __direname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use("assets", express.static(path.join(__direname, 'public/assets')));
+
+//storage config
+ const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "public/assets");
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname)
+    }
+ });
+ 
