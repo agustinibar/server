ae0e72c24d133e2ba5e9c32c78b4da3c17919748
agustinibar <agustinibarperrotta@gmail.com>
2023-10-04 08:12:15 -0300

    user controllers fix

--- controllers/users.js
+++ controllers/users.js
@@ -15,25 +15,25 @@ export const getUser = async(req, res)=>{
 };
 
 
-export const getUserFriends = async(req, res)=>{
-    try {
-        const { id } = req.params;
-        const user = await User.findById(id);
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
 
-        const friends = await Promise.all(
-            user.friends.map((id)=> User.findById(id))
-        );
-
-        const formattedFriends = friends.map(
-            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-                return { _id, firstName, lastName, occupation, location, picturePath }
-            }
-        );
-        res.status(200).json({ formattedFriends });
-    } catch (error) {
-        res.status(404).json({ message: error.message})
-    }
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 };
+
 
 
 // export const addRemoveFriend = async(req, res)=>{
